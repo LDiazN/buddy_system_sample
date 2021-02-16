32b9904c4eae2482c1512152cb1a20a600e418ad
Luis Diaz <ldiazn98@gmail.com>
2021-02-16 18:33:15 -0400

    initial version for free and allocate

--- buddy_system/src/memory_manager/mod.rs
+++ buddy_system/src/memory_manager/mod.rs
@@ -49,8 +49,63 @@ impl MemoryManager {
         manager
     }
 
-    pub fn allocate() {
+    pub fn allocate(&mut self, name : String, size : usize) -> Result<Address, MemoryError> {
+        
+        // Check if too much requested memory to even bother
+        if  size > self.memory.len()    { return Err(MemoryError::OutOfMemory) }
 
+        // Check if name is defined
+        if  self.is_symbol(&name)       { return Err(MemoryError::AlreadyExistingSymbol) }
+
+        // run our algorithm
+        let chunk_size = utils::nearest_upper_2_power(size);
+        for (block_size, list) in &mut self.blocks {
+
+            if *block_size < chunk_size || list.is_empty() { continue }
+
+            // Create a new symbol
+            let pos = list.pop().unwrap();
+            let data = SymbolData {
+                position : pos,
+                size : size,
+                chunk_size : chunk_size,
+                id : self.next_available_id
+            };
+
+            self.names.insert(name, data);
+
+            // update chunks
+            let size = *block_size;
+            self.create_chunks(pos + chunk_size, pos + size - 1);
+
+            return Ok(pos)
+        }
+
+
+        Err(MemoryError::OutOfMemory)
+    }
+
+    pub fn free(&mut self, name : &String) -> Result<(),MemoryError>{
+
+        // get data for this symbol and then delete it 
+        let (chunk_size, position) = {
+            match self.names.get(name) {
+                None => return Err(MemoryError::SymbolNotDefined),
+                Some(d) => (d.chunk_size, d.position)
+            }            
+        };
+
+        self.names.remove(name);
+
+        // return this symbol's memory to the block;
+        let entry = self.blocks.entry(chunk_size).or_insert(vec![]);
+        entry.push(position);
+
+        Ok(())
+    }
+
+    pub fn show(&self) {
+        
     }
 
     fn create_chunks(&mut self, start : usize, end : usize) {
@@ -75,10 +130,7 @@ impl MemoryManager {
         entry.push(position);
     }
 
-    fn insert_symbol(&mut self, name : String, symbol_data : SymbolData) -> Result<(), MemoryError>{
-
-        self.names.insert(name, symbol_data);
-        return Ok(());
+    pub fn is_symbol(&self, name : &String) -> bool{
+        self.names.contains_key(name)
     }
-
 }
